4ac5926754761154163676f212dce0ffb49146fb
olesia-bondarchuk <olesia.bondarchuk@outlook.com>
2022-07-26 12:38:28 -0600

    LC-21 Fixed pylint bug

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from helpers.list_node import ListNode
-
 from lc_21_merge_two_sorted_lists.merge_two_sorted_lists import Solution
 
-
 if __name__ == '__main__':
-    result = Solution().mergeTwoLists(ListNode(1, ListNode(2, ListNode(4))), 
+    result = Solution().mergeTwoLists(ListNode(1, ListNode(2, ListNode(4))),
     ListNode(1, ListNode(3, ListNode(4))))
     print(result)
